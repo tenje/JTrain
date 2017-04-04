d1a44259c5507770f4857ee7a41f13c2e77d5dbf
Jonas T <contact@tenje.org>
2017-04-04 13:51:56 +0200

    Station now stores define daccessories

--- JTrain - DCC++/src/org/tenje/jtrain/dccpp/server/DccppStation.java
+++ JTrain - DCC++/src/org/tenje/jtrain/dccpp/server/DccppStation.java
@@ -17,20 +17,33 @@ package org.tenje.jtrain.dccpp.server;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.tenje.jtrain.AccessoryDecoderAddress;
+import org.tenje.jtrain.OutputPinAddress;
 import org.tenje.jtrain.dccpp.LocalPacketBroker;
 import org.tenje.jtrain.dccpp.Packet;
 import org.tenje.jtrain.dccpp.PacketBroker;
 import org.tenje.jtrain.dccpp.PacketBuilder;
 import org.tenje.jtrain.dccpp.PacketFactory;
 import org.tenje.jtrain.dccpp.PacketListener;
+import org.tenje.jtrain.dccpp.PacketOutputPin;
+import org.tenje.jtrain.dccpp.PacketOutputPinDefine;
 import org.tenje.jtrain.dccpp.PacketReadCurrent;
 import org.tenje.jtrain.dccpp.PacketReadStationState;
+import org.tenje.jtrain.dccpp.PacketSensor;
+import org.tenje.jtrain.dccpp.PacketSensorDefine;
+import org.tenje.jtrain.dccpp.PacketTurnout;
+import org.tenje.jtrain.dccpp.PacketTurnoutDefine;
 import org.tenje.jtrain.dccpp.impl.AbstractPacket;
+import org.tenje.jtrain.dccpp.impl.PacketOutputPinDefineImpl;
 import org.tenje.jtrain.dccpp.impl.PacketReadCurrentImpl;
 import org.tenje.jtrain.dccpp.impl.PacketReadStationStateImpl;
+import org.tenje.jtrain.dccpp.impl.PacketSensorDefineImpl;
 import org.tenje.jtrain.dccpp.impl.PacketStationInfoImpl;
+import org.tenje.jtrain.dccpp.impl.PacketTurnoutDefineImpl;
 
 /**
  * A {@link DccppStation} is the connection between a DCC++ controller (e.g.
@@ -38,7 +51,10 @@ import org.tenje.jtrain.dccpp.impl.PacketStationInfoImpl;
  * accessories (e.g. trains, turnouts, ...). {@link Packet}s received from a
  * controller are forwarded to each connected accessory. {@link Packet}s
  * received from an accessory are forwarded to each connected controller. The
- * packet data is not validated.
+ * packet data is not validated.<br />
+ * Data of {@link PacketTurnoutDefine}, {@link PacketOutputPinDefine} and
+ * {@link PacketSensorDefine} received from controller brokers are stored and
+ * sent to accessory brokers after connecting to the station.
  * 
  * @author Jonas Tennié
  */
@@ -69,6 +85,10 @@ public class DccppStation implements PacketListener {
 	};
 
 	private final DccppServerSocket controllerSocket, accessorySocket;
+
+	private final Set<TurnoutEntry> turnuts = new HashSet<>();
+	private final Set<OutputPinEntry> outputPins = new HashSet<>();
+	private final Set<SensorEntry> sensors = new HashSet<>();
 
 	/**
 	 * Constructs a new {@link DccppStation} and starts a server socket for
@@ -103,35 +123,123 @@ public class DccppStation implements PacketListener {
 		accessorySocket.addPacketListener(this);
 		controllerSocket.setPacketFactory(PACKET_FACTORY);
 		accessorySocket.setPacketFactory(PACKET_FACTORY);
+		accessorySocket.addSocketListener(new SocketListener() {
+			@Override
+			public void socketEvent(SocketEvent event) {
+				try {
+					for (TurnoutEntry entry : turnuts) {
+						accessorySocket.sendPacket(
+								new PacketTurnoutDefineImpl(entry.id, entry.address),
+								event.getBroker());
+					}
+					for (OutputPinEntry entry : outputPins) {
+						accessorySocket.sendPacket(new PacketOutputPinDefineImpl(entry.id,
+								entry.address, entry.flag), event.getBroker());
+					}
+					for (SensorEntry entry : sensors) {
+						accessorySocket.sendPacket(new PacketSensorDefineImpl(entry.id,
+								entry.address, entry.pullUp), event.getBroker());
+					}
+				}
+				catch (IOException ex) {}
+			}
+		});
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void packetReceived(Packet packet, PacketBroker sender,
 			LocalPacketBroker receiver) throws IOException {
-		System.out.println(packet);
-		if (packet.getTypeChar() == PacketReadCurrent.TYPE_CHAR) {
-			// TODO Fix
-			receiver.sendPacket(new AbstractPacket('a', Arrays.asList("0")) {}, sender);
-		}
-		else if (packet.getTypeChar() == PacketReadStationState.TYPE_CHAR) {
-			// TODO Send power state (and other status packets?)
-			receiver.sendPacket(new PacketStationInfoImpl("Java DCC++ Base Station"),
-					sender);
-		}
-		else {
-			if (receiver == controllerSocket) {
-				for (PacketBroker accessoryBroker : accessorySocket
-						.getConnectedBrokers()) {
-					accessorySocket.sendPacket(packet, accessoryBroker);
+		System.out.println("Packet received: " + packet.getTypeChar() + ": "
+				+ packet.getRawParameters());
+		switch (packet.getTypeChar()) {
+			case PacketReadCurrent.TYPE_CHAR: {
+				// TODO Fix
+				receiver.sendPacket(new AbstractPacket('a', Arrays.asList("0")) {},
+						sender);
+				return;
+			}
+			case PacketReadStationState.TYPE_CHAR: {
+				// TODO Send power state (and other status packets?)
+				receiver.sendPacket(new PacketStationInfoImpl("Java DCC++ Base Station"),
+						sender);
+				return;
+			}
+			case PacketTurnout.TYPE_CHAR: {
+				if (receiver == controllerSocket
+						&& packet instanceof PacketTurnoutDefine) {
+					PacketTurnoutDefine defPacket = (PacketTurnoutDefine) packet;
+					TurnoutEntry entry = new TurnoutEntry();
+					entry.id = defPacket.getId();
+					entry.address = defPacket.getAddress();
+					turnuts.add(entry);
 				}
 			}
-			else if (receiver == accessorySocket) {
-				for (PacketBroker controllerBroker : controllerSocket
-						.getConnectedBrokers()) {
-					controllerSocket.sendPacket(packet, controllerBroker);
+			break;
+			case PacketOutputPin.TYPE_CHAR: {
+				if (receiver == controllerSocket
+						&& packet instanceof PacketOutputPinDefine) {
+					PacketOutputPinDefine defPacket = (PacketOutputPinDefine) packet;
+					OutputPinEntry entry = new OutputPinEntry();
+					entry.id = defPacket.getId();
+					entry.address = defPacket.getAddress();
+					entry.flag = defPacket.getFlags();
+					outputPins.add(entry);
 				}
 			}
+			break;
+			case PacketSensor.TYPE_CHAR: {
+				if (receiver == controllerSocket
+						&& packet instanceof PacketSensorDefine) {
+					PacketSensorDefine defPacket = (PacketSensorDefine) packet;
+					SensorEntry entry = new SensorEntry();
+					entry.id = defPacket.getId();
+					entry.address = defPacket.getAddress();
+					entry.pullUp = defPacket.usePullUp();
+					sensors.add(entry);
+				}
+			}
+			break;
 		}
+		if (receiver == controllerSocket) {
+			for (PacketBroker accessoryBroker : accessorySocket.getConnectedBrokers()) {
+				accessorySocket.sendPacket(packet, accessoryBroker);
+			}
+		}
+		else if (receiver == accessorySocket) {
+			for (PacketBroker controllerBroker : controllerSocket.getConnectedBrokers()) {
+				controllerSocket.sendPacket(packet, controllerBroker);
+			}
+		}
+	}
+
+	private static class Entry {
+		int id;
+
+		@Override
+		public int hashCode() {
+			return id;
+		}
+
+		// No null check or type check required
+		@Override
+		public boolean equals(Object obj) {
+			return id == ((Entry) obj).id;
+		}
+	}
+
+	private static class TurnoutEntry extends Entry {
+		AccessoryDecoderAddress address;
+	}
+
+	private static class OutputPinEntry extends Entry {
+		OutputPinAddress address;
+		int flag;
+	}
+
+	private static class SensorEntry extends Entry {
+		AccessoryDecoderAddress address;
+		boolean pullUp;
 	}
 
 }
